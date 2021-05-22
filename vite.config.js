9fef760367a1458f86b94ad93c64364ffa1d28d9
croatia <wuli.croatia@foxmail.com>
2021-05-22 20:26:17 +0800

    chore: 修改打包配置

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://cdn.jsdelivr.net/gh/croatiaparanoia/vue-playground@gh-pages/',
 });
